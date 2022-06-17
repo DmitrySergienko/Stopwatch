a28fe881a5a3552069f87cd8ee3fbed34400785d
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-17 11:11:50 +0400

    6 ViewModel and Coroutines implemented (but app not run - no idea why)

--- app/src/main/java/com/gb/stopwatch/di/appModule.kt
+++ app/src/main/java/com/gb/stopwatch/di/appModule.kt
@@ -7,7 +7,7 @@ import org.koin.dsl.module
 
 val appModule = module {
 
-   viewModel { MainActivityViewModel(stopwatchListOrchestrator = get()) }
+   viewModel { MainActivityViewModel(get()) }
 
 
 }
--- app/src/main/java/com/gb/stopwatch/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/gb/stopwatch/presentation/MainActivityViewModel.kt
@@ -1,39 +1,14 @@
 package com.gb.stopwatch.presentation
 
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.gb.stopwatch.data.StopwatchListOrchestrator
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-
-<<<<<<< HEAD
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-
-private const val DEFAULT_PREVIOUS_TIME = 0.00f
-
-
-class MainActivityViewModel(
-    private val stopwatchListOrchestrator: StopwatchListOrchestrator
-) :
-    ViewModel() {
-
-    private val timeData: MutableLiveData<StateFlow<String>> by lazy { MutableLiveData<StateFlow<String>>() }
-
-    fun getTime() = viewModelScope.launch {
-
-        val time = stopwatchListOrchestrator.ticker
-        timeData.postValue(time)
-=======
 import com.gb.stopwatch.domain.StopwatchStateHolder
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 
 class MainActivityViewModel(
@@ -49,7 +24,7 @@ class MainActivityViewModel(
             mutableTicker.value = stopwatchStateHolder.getStringTimeRepresentation()
             delay(20)
         }
->>>>>>> f0e15f9 (4.Try to get data using coroutines and viewModel)
+
 
     }
 
